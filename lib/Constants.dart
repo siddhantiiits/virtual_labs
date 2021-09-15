bfec800fe3cf23489d94a9609f701fe0abdc8eb4
siddhantiiits <54790933+siddhantiiits@users.noreply.github.com>
2021-09-15 13:22:03 +0530

    UI changes

---
+++ lib/Constants.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+const Color grey = Colors.black54;
+const Color button_background_color = Colors.white12;
--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,11 @@
 import 'package:flutter/material.dart';
-
+import 'package:virtual_labs/Constants.dart';
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+  runApp(MaterialApp(home: MyHomePage()));
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
 
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
@@ -41,7 +16,7 @@ class MyHomePage extends StatefulWidget {
   // used by the build method of the State. Fields in a Widget subclass are
   // always marked "final".
 
-  final String title;
+//  final String title;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -73,7 +48,24 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Virtual Labs'),
+        centerTitle: false,
+        backgroundColor: grey,
+        actions: <Widget>[
+          ElevatedButton(onPressed: () {}, child: Text(" Subjects "),
+            style: ButtonStyle(backgroundColor: MaterialStateProperty.all<Color>(button_background_color),),),
+          SizedBox(width: 10,),
+          ElevatedButton(onPressed: () {}, child: Text("Developers"),
+            style: ButtonStyle(backgroundColor: MaterialStateProperty.all<Color>(button_background_color),),),
+          SizedBox(width: 10,),
+          ElevatedButton(onPressed: () {}, child: Text("Contact Us"),
+            style: ButtonStyle(backgroundColor: MaterialStateProperty.all<Color>(button_background_color),),),
+          SizedBox(width: 30,),
+
+        ],
+
+
+
       ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
